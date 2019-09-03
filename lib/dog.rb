244a30f8e10e37829cf06bfcd97e26abed2f8aec
Marianne Pyon <mariannepyon@gmail.com>
2019-09-03 12:56:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Dog
-  # code goes here
+
+@@all = []
+
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
